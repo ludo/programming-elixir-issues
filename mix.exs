c03f3c6c25fb22780b489140f632b637952fcf0d
Ludo van den Boom <ludo@cubicphuse.nl>
2013-10-02 12:40:36 +0200

    Added httpotion dep

--- mix.exs
+++ mix.exs
@@ -16,6 +16,7 @@ defmodule Issues.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    []
+    [ {:httpotion,"0.2.2",[github: "myfreeweb/httpotion"]}
+    ]
   end
 end
