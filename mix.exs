c65663004cbd5a8fa75c11f88a377f26c5dafa4e
Ludo van den Boom <ludo@cubicphuse.nl>
2013-10-06 14:43:11 +0200

    Add ExDoc (12.12)

--- mix.exs
+++ mix.exs
@@ -4,6 +4,8 @@ defmodule Issues.Mixfile do
   def project do
     [ app: :issues,
       version: "0.0.1",
+      name: "Issues",
+      source_url: "https://github.com/ludo/programming-elixir-issues",
       elixir: "~> 0.10.2",
       deps: deps ]
   end
@@ -16,7 +18,8 @@ defmodule Issues.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [ {:httpotion,"0.2.2",[github: "myfreeweb/httpotion"]},
+    [ {:ex_doc, github: "elixir-lang/ex_doc"},
+      {:httpotion,"0.2.2",[github: "myfreeweb/httpotion"]},
       {:jsonex,"2.0",[github: "marcelog/jsonex", tag: "2.0"]}
     ]
   end
